d885b360375e38dad9f4ea360e3192d20a5dcc96
masuod-abbasian <masuod.abbasian@gmail.com>
2022-01-30 14:09:19 -0800

    BasicAuthentication in api app

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -131,5 +131,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'api.permissions.IsStaffOrReadOnly',
     ],
-    
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        # jwt
+        # knox
+        # oauth
+    ]
 }
