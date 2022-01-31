635111f8f3b7975ae10ddab50b38246d3e1d0326
masuod-abbasian <masuod.abbasian@gmail.com>
2022-01-31 03:16:06 -0800

    add rest Session in api app

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -132,8 +132,8 @@ REST_FRAMEWORK = {
         'api.permissions.IsStaffOrReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         # 'rest_framework.authentication.TokenAuthentication',
         # jwt
         # knox
